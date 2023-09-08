0137b6a2574dc0f418f99b80e23e3160ffd0c43e
nicoshakalis <nshakalis@udesa.edu.ar>
2023-09-08 20:05:28 -0300

    comienzo favoritos

--- src/components/MovieCard/MoviesCard.js
+++ src/components/MovieCard/MoviesCard.js
@@ -1,17 +1,51 @@
-import React from 'react'
+import React, {component} from 'react';
 // falta linkear al detalle
 
 
-function MovieCard(props) {
+class MovieCard extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            textoBoton: "Agregar a favoritos",
+            personajes : [],
+        }
+    }
+
+    componentDidMount(){
+       let recuperoStorage = localStorage.getItem('favoritos')
+       if (recuperoStorage !== null){
+         let favoritos = JSON.parse(recuperoStorage);
+         if(favoritos.includes(this.props.data.id)){
+            this.setState({
+                textoBoton: "Quitar de favoritos"
+            })
+        }
+       }
+    }
+
+
+    ModificarFavoritos(id){
+        let favoritos = [];
+        favoritos.push(id);
+        let favoritosString = JSON.stringify(favoritos);
+        localStorage.setItem('favoritos', favoritosString)
+
+        this.setState({
+            textoBoton: "Quitar de favoritos"
+        })
+    }
+    
+
+    render(){
     return(
-    props.data.map(pelicula => {
   
     <article class = "container">
-        <img src={props.data.img}  alt="Foto ${pelicula.original_title}" className="foto-home" />
-        <p> ${props.data.original_title} </p>
-        <p>${props.data.descripcion}</p>
+        <img src={this.props.data.img}  alt="Foto ${pelicula.original_title}" className="foto-home" />
+        <button onClick={()=>this.ModificarDeFavoritos(this.props.data.id)} type='button'>{this.state.textoBoton}</button>
+        <p> {this.props.data.original_title} </p>
+        <p>{this.props.data.descripcion}</p>
     </article>
-    })
     )
+}
 }
 export default MovieCard
--- src/components/SeriesCard/SeriesCard.js
+++ src/components/SeriesCard/SeriesCard.js
@@ -1,18 +1,56 @@
-import React from 'react'
-function SeriesCard(props) {
-   let SeriesCard = props.data.map(function(serie){
-    let path = serie.poster_path
-    let img = `https://image.tmdb.org/t/p/w500/${path}`
-    return(
-    props.data.map(serie => {
-    <article class = "container">
-        <a href="./detalle-pelis.html?movie_id=${id}">
-        <img src= {props.data.img}  alt="Foto ${pelicula.original_title}" className="foto-home" /></a> //props .data o .serie?
-        <p> {props.data.original_title} </p>
-        <p>{props.data.descripcion}</p>
-    </article>
+import React, {component} from 'react';
+
+class SeriesCard extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            textoBoton: "Agregar a favoritos",
+            personajes : [],
+        }
+    }
+
+
+// function SeriesCard(props) {
+//      let SeriesCard = props.data.map(function(serie){
+//     let path = serie.poster_path
+//     let img = `https://image.tmdb.org/t/p/w500/${path}`
+// })
+// }
+componentDidMount(){
+    let recuperoStorage = localStorage.getItem('favoritos')
+    if (recuperoStorage !== null){
+      let favoritos = JSON.parse(recuperoStorage);
+      if(favoritos.includes(this.props.data.id)){
+         this.setState({
+             textoBoton: "Quitar de favoritos"
+         })
+     }
+    }
+ }
+
+ModificarFavoritos(id){
+    let favoritos = [];
+    favoritos.push(id);
+    let favoritosString = JSON.stringify(favoritos);
+    localStorage.setItem('favoritos', favoritosString)
+    this.setState({
+        textoBoton: "Quitar de favoritos"
     })
-    )
-   })
+
+}
+
+
+
+    render(){
+        props.data.map(serie => {
+            <article class = "container">
+                <a href="./detalle-pelis.html?movie_id=${id}">
+                <img src= {props.data.img}  alt="Foto ${pelicula.original_title}" className="foto-home" /></a> //props .data o .serie?
+                <button onClick={()=>this.ModificarFavoritos(this.props.data.id)} type='button'>{this.state.textoBoton}</button>
+                <p> {props.data.original_title} </p>
+                <p>{props.data.descripcion}</p>
+            </article>
+            })
+    }
 }
 export default SeriesCard
