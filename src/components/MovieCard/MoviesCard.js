03c67b21b8c74a7e9bfecc7cb011ae661749a497
simon10p <sdiezpena@udesa.edu.ar>
2023-09-08 10:31:15 -0300

    actualizacion

--- src/components/MovieCard/MoviesCard.js
+++ src/components/MovieCard/MoviesCard.js
@@ -4,12 +4,14 @@ function MovieCard(props) {
     let path = pelicula.poster_path
     let img = `https://image.tmdb.org/t/p/w500/${path}`
     return(
-        <article class = "container">
+    props.data.map(pelicula => {
+    <article class = "container">
         <a href="./detalle-pelis.html?movie_id=${id}">
         <img src="${img}"  alt="Foto ${pelicula.original_title}" className="foto-home" /></a>
         <p> ${pelicula.original_title} </p>
         <p>${pelicula.release_date}</p>
     </article>
+    })
     )
    })
 }
