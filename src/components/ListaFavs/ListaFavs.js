d5a9a631e7072975894df8ec9cdf2e50c7901fe6
MateoPode <mpodetti@udesa.edu.ar>
2023-09-14 15:30:48 -0300

    Errores en favs

--- src/components/ListaFavs/ListaFavs.js
+++ src/components/ListaFavs/ListaFavs.js
@@ -7,31 +7,35 @@ class ListaFavs extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      favorita: [],
+      favoritaPelicula: [],
       favoritaSerie: []
     };
   }
   componentDidMount() {
-    let favoritos = [];
+    let favoritosPelis = [];
     let favoritaSerie =[];
     let recuperoStorage = localStorage.getItem("favoritos");
     let recuperoStorageSeries = localStorage.getItem("favoritosSeries")
     if (recuperoStorage !== null && recuperoStorageSeries !== null) {
-      favoritos = JSON.parse(recuperoStorage);
+      favoritosPelis = JSON.parse(recuperoStorage);
       favoritaSerie = JSON.parse(recuperoStorageSeries)
     }
-    favoritos.map((id) => {
+    favoritosPelis.map((id) => {
       fetch(
         `https://api.themoviedb.org/3/movie/${id}?api_key=2a3601e42fea0b8cec36fb4c1999c023&language=en-US`
       )
         .then((response) => response.json())
         .then((favorita) => {   
-          let listaFavoritos = this.state.favorita;
+          let listaFavoritos = this.state.favoritaPelicula;
+          console.log(listaFavoritos);
           listaFavoritos.push(favorita);
+          console.log(listaFavoritos);
           this.setState({ favorita: listaFavoritos });
         })
         .catch(error => console.log(error));
     });
+
+
     favoritaSerie.map((idSerie) =>{
       fetch(
         `https://api.themoviedb.org/3/tv/${idSerie}?api_key=2a3601e42fea0b8cec36fb4c1999c023&language=en-U`
@@ -49,13 +53,13 @@ class ListaFavs extends Component {
   render() {
     return (
       <section className="container_padre">
-        {this.state.favorita.length === 0 ? 
+        {this.state.favoritaPelicula.length === 0 ? 
           <p>No tiene favoritos guardados</p>
          : 
           <React.Fragment>
          <h1>Tus peliculas favoritas:</h1>
           <div>
-            {this.state.favorita.map((unPelicula) => (
+            {this.state.favoritaPelicula.map((unPelicula) => (
           <MoviesCard  key={unPelicula.id} datosPelicula={unPelicula} img={unPelicula.poster_path} />
             ))}
             </div> 
